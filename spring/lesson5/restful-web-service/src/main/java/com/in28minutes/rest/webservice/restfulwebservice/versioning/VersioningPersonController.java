2521a8ad27a49e08336f5bd045e04d6eb1ba6902
onlySaying <48788534+onlySaying@users.noreply.github.com>
2024-04-02 09:15:26 +0900

    spring study

--- spring/lesson5/restful-web-service/src/main/java/com/in28minutes/rest/webservice/restfulwebservice/versioning/VersioningPersonController.java
+++ spring/lesson5/restful-web-service/src/main/java/com/in28minutes/rest/webservice/restfulwebservice/versioning/VersioningPersonController.java
@@ -6,9 +6,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
-	@GetMapping("v1/person")
-	public Person gerFirstVersionOfPerson()
+	@GetMapping("/v1/person")
+	public PersonV1 gerFirstVersionOfPerson()
+	{
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping("/v2/person")
+	public PersonV2 gerSecondVersionOfPerson()
+	{
+		return new PersonV2(new Name("BOB", "Charlie"));
+	}
+	
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter()
+	{
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping("/v2/person")
+	public PersonV2 getSecondVersionOfPersonRequestParameter()
+	{
+		return new PersonV2(new Name("BOB", "Charlie"));
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader()
+	{
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader()
+	{
+		return new PersonV2(new Name("BOB", "Charlie"));
+	}
+	
+	@GetMapping(path = "/person/header/accept", produces = 
+			"application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAcceptHeader()
 	{
 		return new PersonV1("Bob Charlie");
 	}
 }
+
+
