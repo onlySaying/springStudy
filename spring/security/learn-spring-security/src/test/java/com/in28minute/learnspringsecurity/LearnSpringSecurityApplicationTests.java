8c47e0c4aedd62dc84fb28add1d2c68a01788752
onlySaying <48788534+onlySaying@users.noreply.github.com>
2024-04-09 14:58:09 +0900

    security study

---
+++ spring/security/learn-spring-security/src/test/java/com/in28minute/learnspringsecurity/LearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minute.learnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
